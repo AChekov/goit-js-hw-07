6af0415ac1c8ed493fe10fb8950d2e7b04093f15
AChekov <4ekoff@gmail.com>
2021-11-23 01:20:30 +0200

    add gallery animation

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const samplesMarkup = createGallerySample(galleryItems);
+
+let instance;
+
+galleryContainer.insertAdjacentHTML("beforeend", samplesMarkup);
+
+galleryContainer.addEventListener("click", onGallerySampleClick);
+
+function createGallerySample(sample) {
+  return sample
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+    })
+    .join("");
+}
+
+function onGallerySampleClick(evt) {
+  evt.preventDefault();
+  openModal(evt);
+  closeModal();
+}
+
+function openModal(evt) {
+  instance = basicLightbox.create(`
+  <img src="${evt.target.dataset.source}">
+`);
+
+  instance.show();
+}
+
+function closeModal() {
+  document.addEventListener("keydown", (evt) => {
+    if (evt.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  });
+}
